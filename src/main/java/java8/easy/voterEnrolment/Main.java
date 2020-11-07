14a79cb77f34868107b35732e1aa8de3c634f5ac
praveeniiht <64822037+praveeniiht@users.noreply.github.com>
2020-11-07 15:55:41 +0530

    Add files via upload

---
+++ src/main/java/java8/easy/voterEnrolment/Main.java
@@ -0,0 +1,21 @@
+package java8.easy.voterEnrolment;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+	public static boolean calculateAge(String birthDate) throws ParseException
+	{
+			// write the code to calculate the age 
+			 return false;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		// write the acutal code to check if he is eligible or not
+	}
+}
